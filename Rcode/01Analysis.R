9a51d4669b65c889f7283bd44150ed07e12096fa
longkhuong <longkhuong.yds@gmail.com>
2024-12-02 09:41:34 -0500

    add logistic

--- Rcode/01Analysis.R
+++ Rcode/01Analysis.R
@@ -32,9 +32,9 @@ df <- df |>
     interval = factor(interval, labels = c("1st", "<24months", "24_47months", "48months")),
     ch_size_birth = factor(ch_size_birth, labels = c("very small", "smaller",
                                                      "average", "larger", "very large")),
-    anc_cat3 = factor(anc_cat3, labels = c("ANC<4", "ANC4-7", "ANC8+")),
-    anc_cat3_cut15 = factor(anc_cat3_cut15, labels = c("ANC<4", "ANC4-7", "ANC8-15")),
-    anc_cat3_cut12 = factor(anc_cat3_cut12, labels = c("ANC<4", "ANC4-7", "ANC8-12")),
+    anc_cat3 = factor(anc_cat3, labels = c("ANCless4", "ANC4-7", "ANC8+")),
+    anc_cat3_cut15 = factor(anc_cat3_cut15, labels = c("ANCless4", "ANC4-7", "ANC8-15")),
+    anc_cat3_cut12 = factor(anc_cat3_cut12, labels = c("ANCless4", "ANC4-7", "ANC8-12")),
     mom_age_cat = factor(mom_age_cat, labels = c("15-19", "20-24", "25-29", "30-34",
                                                  "35-39", "40-44", "45-49")),
     marital = factor(marital, labels = c("Married", "Other")),
@@ -43,7 +43,16 @@ df <- df |>
                                            "High school", "College")),
     wealth = factor(wealth, labels = c("Poorest", "Poorer", "Middle", "Richer", "Richest")),
     placeofres = factor(placeofres, labels = c("Urban", "Rural")),
-    delivery = factor(delivery, labels = c("Normal", "C-Section"))
+    delivery = factor(delivery, labels = c("Normal", "C-Section")),
+    birthorder = relevel(birthorder, "1st"),
+    interval = relevel(interval, "1st"),
+    ch_size_birth = relevel(ch_size_birth, "very small"),
+    anc_cat3 = relevel(anc_cat3, "ANCless4"),
+    anc_cat3_cut15 = relevel(anc_cat3_cut15, "ANCless4"),
+    anc_cat3_cut12 = relevel(anc_cat3_cut12, "ANCless4"),
+    mom_age_cat = relevel(mom_age_cat, "15-19"),
+    mom_educ = relevel(mom_educ, "No education"),
+    wealth = relevel(wealth, "Poorest")
   )
 
 
@@ -154,11 +163,67 @@ write.csv(results_df, "Results/percentages_by_varlist.csv", row.names = TRUE, na
 
 
 
+#----- Table 3: Logistic
+logi_neo <- svyglm(neonatal ~ ch_sex + birthorder + delivery + 
+                     mom_age_cat + marital + mom_educ + early_preg + 
+                     wealth + water + sanit + placeofres + 
+                     inst_skilled + anc4, design = dhs_design, family = quasibinomial)
+
+logi_inf <- svyglm(infant ~ ch_sex + birthorder + delivery + 
+                     mom_age_cat + marital + mom_educ + early_preg + 
+                     wealth + water + sanit + placeofres + 
+                     inst_skilled + anc4, design = dhs_design, family = quasibinomial)
+
+logi_u5 <- svyglm(under5 ~ ch_sex + birthorder + delivery + 
+                     mom_age_cat + marital + mom_educ + early_preg + 
+                     wealth + water + sanit + placeofres + 
+                     inst_skilled + anc4, design = dhs_design, family = quasibinomial)
 
 
+summary(logi_neo)
+summary(logi_inf)
+summary(logi_u5)
+
+coef_tab1 <- summary(logi_neo)$coefficients |>
+  as.tibble(rownames = "Parameter")|>
+  filter(Parameter != "(Intercept)")
+
+coef_tab2 <- summary(logi_inf)$coefficients|>
+  as.tibble(rownames = "Parameter")|>
+  filter(Parameter != "(Intercept)")
+
+coef_tab3 <- summary(logi_u5)$coefficients|>
+  as.tibble(rownames = "Parameter")|>
+  filter(Parameter != "(Intercept)")
+
+coef_tab_all <- rbind(coef_tab1, coef_tab2, coef_tab3) 
+
+coef_tab_all$model <- rep(c("NM", "IM", "U5M"), each = nrow(coef_tab1))
+
+# Calculate OR and 95%CI
+coef_tab_all <- coef_tab_all |>
+  mutate(
+    OR = exp(Estimate),
+    lb = exp(Estimate - 1.96*`Std. Error`),
+    ub = exp(Estimate + 1.96*`Std. Error`)
+  )
 
 
+# Forest plot
+dodge <- position_dodge(width = 0.5)
 
-
-
+coef_tab_all |>
+  ggplot(aes(x = Parameter, y = OR, ymin = lb, ymax = ub, color = model)) + 
+  geom_errorbar(width = 0.5, size = 1, position = dodge) +
+  geom_point(size = 2, shape = 21, fill="white", position = dodge) +
+  geom_hline(yintercept = 1, linetype = 2, col = "red", size = 1) +
+  scale_color_brewer(palette = "Set1") +
+  coord_flip() +
+  theme_bw() + 
+  theme(legend.position = "top") +
+  labs(
+    x = NULL,
+    y = "OR (95% CI)",
+    color = "Model"
+  )
 
